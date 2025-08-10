e2d84ee90d0f3e32fc37bc5d5041ccaaa69e95df
thoai240699 <thoai240699@gmail.com>
2025-08-10 21:12:40 +0700

    add tabs

---
+++ app/movies/[id].tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const MovieDetails = () => {
+
+    return (<View>
+        <Text >Movie detail</Text>
+    </View>)
+}
+export default MovieDetails
+
---
+++ app/onboarding.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Onboarding = () => {
+    return (<View>
+        <Text >Onboarding</Text>
+    </View>)
+}
+export default Onboarding
+const styles = StyleSheet.create({})

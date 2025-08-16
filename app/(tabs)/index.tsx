fb75833a515f0d4d67f2a30afc9164727499e993
thoai240699 <thoai240699@gmail.com>
2025-08-16 14:05:22 +0700

    add icon and home bg logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { images} from '@/constants/images';
+import { icons } from '@/constants/icons';
+import { Image, ScrollView, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 justify-center items-center"    >
-      <Text className="text-5xl text-purple-500 font-bold">Hi.</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"></Image>
+      <ScrollView className='flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom: 10}}>
+        <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto'></Image>
+      </ScrollView>
     </View>
   );
 }
